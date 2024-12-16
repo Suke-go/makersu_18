898000795dc3bbd599bc2d9a2ada9f10bf427bb1
Kosuke Shimizu <ksk432works@gmail.com>
2024-12-17 02:25:11 +0900

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,12 @@
+// backend/config.js
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admin';
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'makersahaday!';
+const SERVER_PORT = process.env.PORT || 4000; // デプロイ環境では通常PORTが設定されます
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3001'; // フロントエンドのURL
+
 module.exports = {
-    ADMIN_USERNAME: "admin",
-    ADMIN_PASSWORD: "kousuke0816", // 本当はハッシュ化推奨
-    SERVER_PORT: 4000
-  };
-  
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
+  SERVER_PORT,
+  FRONTEND_URL
+};
