ac614830e65772d63bba0a46b4e3381683bd2ffa
Kosuke Shimizu <ksk432works@gmail.com>
2024-12-17 03:17:40 +0900

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
 // バックエンドURLは適宜変更
-export const socket = io("http://localhost:4000", {transports:['websocket']});
+export const socket = io("https://makersu-18.onrender.com", {transports:['websocket']});
